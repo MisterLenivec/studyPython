486304601f403af6e5dfa722c46c85e7fef97527
Dmitry Sharomyshkin <Wormsom@Gmail.com>
2018-08-02 20:41:55 +0300

    practice python stepik c3 s5 task 2 add comment

--- stepic-python-programming/3-5_sys-module.py
+++ stepic-python-programming/3-5_sys-module.py
@@ -1,15 +1,22 @@
-from sys import argv
+# Напишите программу, которая запускается из консоли и печатает значения всех переданных аргументов на экран
+# (имя скрипта выводить не нужно). Не изменяйте порядок аргументов при выводе.
+# Для доступа к аргументам командной строки программы подключите модуль sys и используйте переменную argv из этого модуля.
+# Пример работы программы:
+# > python3 my_solution.py arg1 arg2
+# arg1 arg2
 
-#first solution
+from ys import argv
+
+# first solution
 print(*argv[1:])
 
-#second
+# second
 print(' '.join(argv[1:]))
 
-#third
+# third
 for i in range(1, len(argv)):
-    print(argv[i], end = ' ')
+    print(argv[i], end=' ')
 
-#fourth
+# fourth
 for i in argv[1:]:
-    print(i, end = ' ')
+    print(i, end=' ')
