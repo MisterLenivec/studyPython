93f4c15e38a502fa0cb1f3211020173f6af7c5ca
Dmitry Sharomyshkin <Wormsom@Gmail.com>
2018-09-17 19:45:49 +0300

    stepik python practice c2 s1 task1

---
+++ stepic-python-programming-lvl2/2-1_simple-example-try-except.py
@@ -0,0 +1,13 @@
+# Вашей программе будет доступна функция foo, которая может бросать исключения.
+# Вам необходимо написать код, который запускает эту функцию, затем ловит исключения ArithmeticError, AssertionError,
+# ZeroDivisionError и выводит имя пойманного исключения.
+
+# Очевидное
+try:
+    foo()
+except ZeroDivisionError:
+    print("ZeroDivisionError")
+except ArithmeticError:
+    print("ArithmeticError")
+except AssertionError:
+    print("AssertionError")
