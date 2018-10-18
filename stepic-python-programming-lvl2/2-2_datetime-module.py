ecf5d19d26a9e0ad432a4eb60b20c9a0e9979be8
Dmitry Sharomyshkin <Wormsom@Gmail.com>
2018-10-18 23:58:57 +0300

    stepik python practice c2 s2 task1

---
+++ stepic-python-programming-lvl2/2-2_datetime-module.py
@@ -0,0 +1,21 @@
+# В первой строке дано три числа, соответствующие некоторой дате date -- год, месяц и день.
+# Во второй строке дано одно число days -- число дней.
+# Вычислите и выведите год, месяц и день даты, которая наступит, когда с момента исходной даты date пройдет число дней, равное days.
+# Примечание:
+# Для решения этой задачи используйте стандартный модуль datetime.
+# Вам будут полезны класс datetime.date для хранения даты и класс datetime.timedelta﻿ для прибавления дней к дате.
+# Sample Input 1:
+# 2016 4 20
+# 14
+# Sample Output 1:
+# 2016 5 4
+
+# First solution
+import datetime
+
+(y, m, d) = [int(n) for n in input().split()]
+date_now = datetime.date(y, m, d)
+plus_day = datetime.timedelta(days=int(input()))
+days_later = date_now + plus_day
+print(days_later.strftime('%Y %-m %-d'))
+# print(days_later.strftime('%Y %#m %#d')) # Для винды убираем нули из месяца и дня
