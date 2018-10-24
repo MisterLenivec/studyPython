48531ee145d47f9f5ba9b2891b177a2a1c901960
Dmitry Sharomyshkin <Wormsom@Gmail.com>
2018-10-24 03:01:41 +0300

    practice python stepik c2 s6 task 2 add new solution

--- stepic-python-programming/2-6_sequences.py
+++ stepic-python-programming/2-6_sequences.py
@@ -11,3 +11,10 @@ for i in range(a+1):
         b += 1
         if (b < a+1):
           print(i, end = ' ')
+
+# Second solution
+n, val = '', int(input())
+for i in range(val + 1):
+    n += (str(i) + ' ') * i
+
+print(n[:val * 2])
